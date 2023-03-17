9413c6debc8722c96ed4f8530357da4127232975
vristovski <vaci.ristovski@gmail.com>
2023-03-17 18:25:47 +0100

    Initial commit

---
+++ src/test/java/com/busify/busifyappproject/BusifyAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busify.busifyappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusifyAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
