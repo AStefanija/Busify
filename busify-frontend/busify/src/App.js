2bedd4e1721b510bee5e418bb1b21b6c3b95ae5f
Stefanija Anastasova <stefanijaanastasova3@gmail.com>
2023-09-23 17:48:12 +0200

    Profile View

--- busify-frontend/busify/src/App.js
+++ busify-frontend/busify/src/App.js
@@ -5,7 +5,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 // import Home from "./components/Home"
 // import Reservation from "./components/Reservation";
-// import Profile from "./components/Profile";
+import Profile from "./components/Profile";
 // import FutureTours from "./components/FutureTours";
 // import PastTours from "./components/PastTours";
 // import Route_Review from "./components/Route_Review";
@@ -20,7 +20,7 @@ class App extends Component {
             <Route path="/register" element={<Register />} />
             {/*<Route path="/home" element={<Home />}/>*/}
             {/*<Route path="/reservation" element={<Reservation />}/>*/}
-            {/*<Route path="/profile" element={<Profile />}/>*/}
+            <Route path="/profile" element={<Profile />}/>
             {/*<Route path="/futureTours" element={<FutureTours />}/>*/}
             {/*<Route path="/pastTours" element={<PastTours />}/>*/}
             {/*<Route path="routeReviews" element={<Route_Review />}/>*/}
